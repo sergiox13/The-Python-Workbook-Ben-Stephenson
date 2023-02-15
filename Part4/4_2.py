78e52cdbef8c44446a5ffb4f21de5793328cb86f
sergiox13 <dduck492@gmail.com>
2023-02-15 09:08:35 +0300

    Modified solution 4_2

--- Part4/4_2.py
+++ Part4/4_2.py
@@ -2,7 +2,7 @@ BASE = 4
 EACH = 0.25
 
 def taxi_cost(distance):
-    return BASE + (distance/140 * EACH)
+    return BASE + (distance//140 * EACH)
 
 distance = int(input('Введите расстояние в м: '))
 res = taxi_cost(distance)
