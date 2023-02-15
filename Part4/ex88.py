d7f9ff1b10f2eccfd232ce448c26f3e8151d3ec7
sergiox13 <dduck492@gmail.com>
2023-02-15 12:15:38 +0300

    Modified ex88, added ex89

--- Part4/ex88.py
+++ Part4/ex88.py
@@ -1,11 +1,35 @@
-def median(a, b, c):
-    return a + b + c - min(a, b, c) - max(a, b, c)
+def get_numeral(n):
+    if n == 1:
+        return 'first'
+    elif n == 2:
+        return 'second'
+    elif n == 3:
+        return 'third'
+    elif n == 4:
+        return 'fourth'
+    elif n == 5:
+        return 'fifth'
+    elif n == 6:
+        return 'sixth'
+    elif n == 7:
+        return 'seventh'
+    elif n == 8:
+        return 'eighth'
+    elif n == 9:
+        return 'ninth'
+    elif n == 10:
+        return 'tenth'
+    elif n == 11:
+        return 'eleventh'
+    elif n == 12:
+        return 'twelwth'
+    else:
+        return ''
 
 
-a = int(input('Введите первое значение: '))
-b = int(input('Введите второе значение: '))
-c = int(input('Введите третье значение: '))
+def main():
+    for i in range(1, 13):
+        print(get_numeral(i))
 
-res = median(a, b, c)
-
-print(f'Медиана равна {res}')
+if __name__ == '__main__':
+    main()
